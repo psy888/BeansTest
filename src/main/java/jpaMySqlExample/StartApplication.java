b086e5544bc3ead456a94ec20a5fa8e925f8968b
psy888 <tbilisi.zp.snab@gmail.com>
2020-02-04 16:45:20 +0200

    jdbc test multi thread

--- src/main/java/jpaMySqlExample/StartApplication.java
+++ src/main/java/jpaMySqlExample/StartApplication.java
@@ -18,6 +18,35 @@ public class StartApplication implements CommandLineRunner {
 
 
     public void run(String... args) throws Exception {
+
+        Thread thr1 = new Thread(()->{
+            repository.save(new Book("Book1"));
+            repository.save(new Book("Book2"));
+            repository.save(new Book("Book3"));
+            repository.save(new Book("Java"));
+            repository.save(new Book("Book1"));
+            repository.save(new Book("Book2"));
+            repository.save(new Book("Book3"));
+            repository.save(new Book("Java"));
+            System.out.println("findByName thr1");
+            repository.findByName("Java").forEach(System.out::println);
+            System.out.println("thr1 finish");
+        });
+        Thread thr2 = new Thread(()->{
+            repository.save(new Book("Java"));
+            repository.save(new Book("Ruby"));
+            repository.save(new Book("JS"));
+            repository.save(new Book("Java"));
+            repository.save(new Book("Book2"));
+            repository.save(new Book("Book3"));
+            repository.save(new Book("Java"));
+            repository.save(new Book("Ruby"));
+            System.out.println("findByName thr2");
+            repository.findByName("Java").forEach(System.out::println);
+            System.out.println("thr2 finish");
+        });
+        thr1.start();
+        thr2.start();
         repository.save(new Book("Book1"));
         repository.save(new Book("Book2"));
         repository.save(new Book("Book3"));
